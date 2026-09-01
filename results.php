8d7457fd66a2d930e8db6cad05228e49c4edd610
alex7r259 <alex7r46@gmail.com>
2026-09-01 07:56:34 +0500

    Add stage results shortcode 204

--- results.php
+++ results.php
@@ -1,36 +1,12 @@
 <?php
-
-/*
- * Plugin Name: Результаты соревнований
- */
-
-// Регистрируем пользовательские параметры запроса
-function add_custom_query_vars($vars) {
-    $vars[] = 'season_id';
-    $vars[] = 'event_id';
-    $vars[] = 'class_name';
-    return $vars;
-}
-
-add_filter('query_vars', 'add_custom_query_vars');
-
-add_action( 'admin_menu', 'results_settings', 25 );
-include 'settings.php';
-
-// Правила итогового результата этапа по состояниям СУ.
-include_once __DIR__ . '/includes/section-result-rules.php';
-
-add_shortcode('results_201', 'results_show_new');
-include 'shortcode_201.php';
-
-add_shortcode( 'results_202', 'results_show_single_event' );
-include 'shortcode_202.php';
-
-add_shortcode( 'results_203', 'results_show_top3' );
-include 'shortcode_203.php';
-
-add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_script('jquery');
-});
- 
+/* Plugin Name: Результаты соревнований */
+function add_custom_query_vars($vars) { $vars[]='season_id'; $vars[]='event_id'; $vars[]='class_name'; return $vars; }
+add_filter('query_vars','add_custom_query_vars');
+add_action('admin_menu','results_settings',25); include 'settings.php';
+include_once __DIR__.'/includes/section-result-rules.php';
+add_shortcode('results_201','results_show_new'); include 'shortcode_201.php';
+add_shortcode('results_202','results_show_single_event'); include 'shortcode_202.php';
+add_shortcode('results_203','results_show_top3'); include 'shortcode_203.php';
+add_shortcode('results_204','results_show_stage_sections'); include 'shortcode_204.php';
+add_action('wp_enqueue_scripts',function(){wp_enqueue_script('jquery');});
 ?>
