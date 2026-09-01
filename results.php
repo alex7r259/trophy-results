2840a02c4619cd8c0b622483be204334561c6e6d
alex7r259 <alex7r46@gmail.com>
2026-09-01 07:51:13 +0500

    Load stage result status rules

--- results.php
+++ results.php
@@ -6,9 +6,9 @@
 
 // Регистрируем пользовательские параметры запроса
 function add_custom_query_vars($vars) {
-    $vars[] = 'season_id';  // Добавляем season_id
-    $vars[] = 'event_id'; // Добавляем class_name
-    $vars[] = 'class_name'; // Добавляем class_name
+    $vars[] = 'season_id';
+    $vars[] = 'event_id';
+    $vars[] = 'class_name';
     return $vars;
 }
 
@@ -17,7 +17,9 @@ add_filter('query_vars', 'add_custom_query_vars');
 add_action( 'admin_menu', 'results_settings', 25 );
 include 'settings.php';
 
-// Зарегистрируйте шорткод с обновленной функцией
+// Правила итогового результата этапа по состояниям СУ.
+include_once __DIR__ . '/includes/section-result-rules.php';
+
 add_shortcode('results_201', 'results_show_new');
 include 'shortcode_201.php';
 
@@ -27,7 +29,6 @@ include 'shortcode_202.php';
 add_shortcode( 'results_203', 'results_show_top3' );
 include 'shortcode_203.php';
 
-// Добавляем скрипт jQuery если не подключен
 add_action('wp_enqueue_scripts', function() {
     wp_enqueue_script('jquery');
 });
